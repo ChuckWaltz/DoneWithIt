ead7cf2ff934f74e45bfe1af089f44a842506cd7
Chuck Waltz <chuckwaltz@gmail.com>
2022-07-05 20:42:42 -0400

    add URL for heroku backend to client

--- app/api/client.tsx
+++ app/api/client.tsx
@@ -2,8 +2,9 @@ import { create } from 'apisauce';
 import authStorage from '../auth/storage';
 
 const apiClient = create({
-  baseURL: 'http://192.168.1.87:9000/api',
+  /* baseURL: 'http://192.168.1.87:9000/api', */
   /* baseURL: 'http://192.168.1.253:9001/api', */
+  baseURL: 'https://donewithit-backend-server.herokuapp.com/api',
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
